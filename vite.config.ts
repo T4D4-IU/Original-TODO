951caee39d071b87076caa6e76aed34de188137a
T4D4 .icp <t4d4.icp@gmail.com>
2024-10-01 08:10:35 +0000

    add vite-plugin-pwa and update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate'
+			// TODO add manifest
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
