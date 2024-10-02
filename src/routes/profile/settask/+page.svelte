42c915f7ed715475cfa3dd3f19a9f27eff6c1f40
T4D4 .icp <t4d4.icp@gmail.com>
2024-10-02 08:21:10 +0000

    add /settask +page.svelte

---
+++ src/routes/profile/settask/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import Corbado from '@corbado/web-js';
+    import type {SessionUser} from "@corbado/types";
+    import { onMount } from 'svelte';
+    let user: SessionUser | undefined;
+    onMount(() => {
+        user = Corbado.user
+    });
+    export let data: PageData;
+</script>
+
+<div>
+    {#if user}
+    <h1>Set Task</h1>
+    <form method="POST">
+        <label>
+            add TODO:
+            <input
+            type="text"
+            name="task"
+            autocomplete="off"
+            />
+        </label>
+    </form>
+    {:else}
+    <a href="/">Login</a>
+    {/if}
+</div>
