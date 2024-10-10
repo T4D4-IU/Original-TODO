75beac83b2f041e8425a93773924974746d4bd4f
T4D4 .icp <t4d4.icp@gmail.com>
2024-10-10 07:48:38 +0000

    update table

--- schema.sql
+++ schema.sql
@@ -1,24 +1,17 @@
-CREATE TABLE user_datas (
-    user_id TEXT PRIMARY KEY,
-    user_name TEXT NOT NULL,
+CREATE TABLE TODOS (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL, -- This will be obtained from the external service
+    title TEXT NOT NULL,
+    url TEXT,
+    status TEXT,
     created_at TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id TEXT NOT NULL,
-    title TEXT NOT NULL,
-    url TEXT,
-    status TEXT CHECK(status IN ('todo', 'in_progress', 'completed')) NOT NULL DEFAULT 'todo',
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
-CREATE TABLE task_sessions (
+CREATE TABLE TASK_SESSIONS (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     task_id INTEGER NOT NULL,
     start_time TEXT NOT NULL,
     end_time TEXT,
-    duration INTEGER, -- 秒単位で保存
-    FOREIGN KEY (task_id) REFERENCES tasks(id)
-)
+    duration INTEGER,
+    FOREIGN KEY(task_id) REFERENCES TODOS(id) ON DELETE CASCADE
+);
